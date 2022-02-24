656635f1069b74e0179dfd6783f959beffb2089c
zqiusen <zqiusen@qq.com>
2022-02-24 22:13:50 +0800

    Collection的基本用法02

--- src/basement/CollectionTest01.java
+++ src/basement/CollectionTest01.java
@@ -3,25 +3,37 @@ package basement;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * @Author: zqiusen@qq.com
  * @Date: 2022/2/21 10:37
  */
 public class CollectionTest01 {
-    public static void main(String[] args) {
-        Collection arr = new ArrayList();
-        arr.add(12);
-        arr.add("dsjf");
-        arr.add("45");
-        Iterator it = arr.iterator();
-        while(it.hasNext()){
-            System.out.println(it.next());
-        }
-        arr.remove(12);
-        System.out.println(arr.size());
-        arr.clear();
-        System.out.println(arr.size());
+//    public static void main(String[] args) {
+//        Collection arr = new ArrayList();
+//        arr.add(12);
+//        arr.add("dsjf");
+//        arr.add("45");
+//        Iterator it = arr.iterator();
+//        while(it.hasNext()){
+//            System.out.println(it.next());
+//        }
+//        arr.remove(12);
+//        System.out.println(arr.size());
+//        arr.clear();
+//        System.out.println(arr.size());
+//    }
+public static void main(String[] args) {
+    List list = new ArrayList();
+    list.add("sddf");
+    list.add(123);
+    Iterator it = list.iterator();
+    while(it.hasNext()) {
+        System.out.println(it.next());
     }
-
+    for(Object ele: list){
+        System.out.println(ele);
+    }
+}
 }
